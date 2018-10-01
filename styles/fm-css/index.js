fda61d9b968509e866f7bc6491d44c87192a042d
ferrymen <ferrymen33@gmail.com>
2018-10-01 19:55:00 +0800

    fix: lint-staged
    affects: @ferrymen/fm-css

--- styles/fm-css/index.js
+++ styles/fm-css/index.js
@@ -1,7 +1,7 @@
-module.exports = function () {
-  console.log(33)
-}
+module.exports = function() {
+  console.log(33);
+};
 
-exports.test = function () {
-  console.log('test')
-}
+exports.test = function() {
+  console.log('test');
+};
