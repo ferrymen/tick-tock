c39851ebd7d13ab1d7e664310d4168531de9f644
ferrymen <ferrymen33@gmail.com>
2018-10-02 19:31:55 +0800

    chore: add eslint and prettier
    affects: @ferrymen/fm-css

---
+++ uis/fm-css/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    jest: true
+  },
+  extends: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false,
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        useTabs: false
+      }
+    ]
+  }
+};
--- uis/fm-css/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  console.log(33);
-};
-
-exports.test = function() {
-  console.log('test');
-};
