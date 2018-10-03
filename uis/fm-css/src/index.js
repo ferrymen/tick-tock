67a3183079973677d50d0e9695e64506323a6e57
ferrymen <ferrymen33@gmail.com>
2018-10-03 18:23:15 +0800

    refactor: add addDependencyMessage
    affects: @ferrymen/fm-css, @ferrymen/fm-react

--- uis/fm-css/src/index.js
+++ uis/fm-css/src/index.js
@@ -1,6 +1,24 @@
-import postcss from 'postcss'
+/**
+ * Main
+ */
+import { resolve } from 'path'
 
+import postcss from 'postcss'
+import _ from 'lodash'
+
+import addDependencyMessage from './utils/addDependencyMessage'
+
+/**
+ * @param {string|object} opts - A config file or params
+ * @returns {processor} - A processor that will apply plugins as CSS processors
+ */
 export default postcss.plugin('fm-css', opts => {
-  console.log(opts)
-  return postcss([])
+  const plugins = []
+
+  // opts as path
+  if (!_.isUndefined(opts) && !_.isObject(opts)) {
+    plugins.push(addDependencyMessage(resolve(opts)))
+  }
+
+  return postcss([...plugins])
 })
