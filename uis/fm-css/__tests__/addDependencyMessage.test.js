09e18878773151963efca0e551eb4e5df90ad44f
ferrymen <ferrymen33@gmail.com>
2018-10-03 19:10:25 +0800

    refactor: add addDependencyMessage.test
    affects: @ferrymen/fm-css, @ferrymen/fm-react

--- uis/fm-css/__tests__/addDependencyMessage.test.js
+++ uis/fm-css/__tests__/addDependencyMessage.test.js
@@ -2,6 +2,17 @@ import { resolve } from 'path'
 import postcss from 'postcss'
 import fmCss from '../src'
 
+it('Throw error if config file does not exist', () => {
+  const conf = resolve(`./fixtures/custom-config.js`)
+  try {
+    postcss(fmCss(conf))
+  } catch (error) {
+    expect(() => {
+      throw error
+    }).toThrow(`Specified config file "${conf}" does't exist.`)
+  }
+})
+
 it('Add the right dependency message', () => {
   const conf = resolve(`${__dirname}/fixtures/custom-config.js`)
   // ./ point to root dir(**/*/fm-css/), so use ${__dirname}
